df1d4dbfaaa9c7d6a1251c4ea47263e4ca2c8e3f
Eric Azevedo <azevedoe@oregonstate.edu>
2025-03-03 21:47:56 -0800

    Added test cases for function 2 (my_datetime) from task.py

--- tests.py
+++ tests.py
@@ -1,11 +1,28 @@
 import unittest
+import task
 
 
 class TestCase(unittest.TestCase):
+    def test_1_function_2(self):
+        s = 0  # input
+        expected = "01-01-1970"
+        self.assertEqual(task.my_datetime(s), expected)
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_2_function_2(self):
+        s = 123456789  # input
+        expected = "11-29-1973"
+        self.assertEqual(task.my_datetime(s), expected)
+
+    def test_3_function_2(self):
+        s = 9876543210  # input
+        expected = "12-22-2282"
+        self.assertEqual(task.my_datetime(s), expected)
+
+    def test_4_function_2(self):
+        s = 201653971200  # input
+        expected = "02-29-8360"
+        self.assertEqual(task.my_datetime(s), expected)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
