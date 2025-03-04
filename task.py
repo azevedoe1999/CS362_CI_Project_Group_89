8dc6734c5fad77788cb04959de28040be712d01f
Eric Azevedo <azevedoe@oregonstate.edu>
2025-03-03 18:31:49 -0800

    Created task.py

---
+++ task.py
@@ -0,0 +1,18 @@
+"""
+Reminders:
+1. you are not permitted to use any Python built-in/library
+2. you may write helper functions if you wish to assist with maintaining targeted code
+"""
+
+
+# Function 2
+def my_datetime(num_sec):
+    """
+    This function takes in an integer value that represents the number of seconds since the epoch: January 1st, 1970.
+    The function takes num_sec and converts it to a date and returns it as a string with the following format: MM-DD-YYYY.
+    It has the following specifications:
+        It may be assumed that num_sec will always be an int value
+        It may be assumed that num_sec will always be a non-negative value
+        Must be able to handle leap years
+    """
+    pass
