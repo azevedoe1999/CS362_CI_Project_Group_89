744ab9871784d3aecad15724f27bccc314255fab
Eric Azevedo <azevedoe@oregonstate.edu>
2025-03-08 20:07:06 -0800

    Updated changes to function 2

--- task.py
+++ task.py
@@ -14,6 +14,11 @@ def my_datetime(num_sec):
         It may be assumed that num_sec will always be an int value
         It may be assumed that num_sec will always be a non-negative value
         Must be able to handle leap years
+
+    1 year(nonleap) = 31,536,000s
+    1 year(leap) = 31,622,400s
+    1 day = 86,400s
+    1 month = days_in_month * 86,400s
     """
 
     # set up variables to start of epoch
@@ -22,50 +27,44 @@ def my_datetime(num_sec):
     day = 1
 
     # set up array for days in each month
-    days_in_each_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     # calculate how many years
-    while num_sec >= 31536000:  # seconds in a year
-        # if particular year is a leap year
+    while num_sec >= 31536000:
         if year % 4 == 0 and year % 100 != 0:
-            # subtract seconds in a leap year from num_sec
-            num_sec -= 31622400
+            num_sec = num_sec - 31622400  # is leap year
         elif year % 400 == 0:
-            num_sec -= 31622400
+            num_sec = num_sec - 31622400  # is leap year
         else:
-            # subtract seconds in a regular year from num_sec
-            num_sec -= 31536000
+            num_sec = num_sec - 31536000
         year += 1
 
     # calculate how many months
-    while num_sec >= 86400:  # seconds in a day
-        if month == 2:  # if month is Feburary
-            # if particular year is a leap year
+    while num_sec >= 86400:
+        # gets current month
+        if month == 2:  # month of Feb
             if year % 4 == 0 and year % 100 != 0:
-                days_in_month = 29
+                days = 29  # is leap year
             elif year % 400 == 0:
-                days_in_month = 29
+                days = 29  # is leap year
             else:
-                # not a leap year, so Feb has 28 days
-                days_in_month = 28
+                days = 28
         else:
-            # for all the other months
-            days_in_month = days_in_each_month[month - 1]
+            days = months[month - 1]
 
-        if num_sec >= days_in_month * 86400:
-            # subtract seconds in a given month from num_sec
-            num_sec -= days_in_month * 86400
+        if num_sec >= days * 86400:
+            num_sec = num_sec - (days * 86400)
             month += 1
-            # restart the month once it hit December (12) and add a year
-            if month > 12:
+            if month > 12:  # end of year --> add year & start at month 1 (Jan)
                 month = 1
                 year += 1
         else:
             break
 
     # calculate the day
-    day += num_sec // 86400
-    num_sec %= 86400
+    day = day + (num_sec // 86400)
+
+    # don't care about the remaining seconds
 
     # formate the date
     return f"{month:02d}-{day:02d}-{year}"
