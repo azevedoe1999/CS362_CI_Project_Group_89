93d71f42f2ab034af9ffed026a99de21bcd103a8
Eric Azevedo <azevedoe@oregonstate.edu>
2025-03-10 20:15:18 -0700

    Added function 3 (conv_endian) for review

--- task.py
+++ task.py
@@ -70,3 +70,75 @@ def check_for_leap_year(year):
         return True
 
     return False
+
+
+# function 3
+def conv_endian(num, endian="big"):
+    """
+    This function must have the following header: def conv_endian(num, endian='big').
+    This function takes in an integer value as num and converts it to a hexadecimal number.
+    The endian type is determined by the flag endian.
+    The function will return the converted number as a string.
+    It has the following specifications:
+        It may be assumed that num will always be an integer
+        Must be able to handle negative values for num
+        A value of big for endian will return a hexadecimal number that is big-endian
+        A value of little for endian will return a hexadecimal number that is little-endian
+        Any other values of endian will return None (n.b. this is not a string, but the actual None value)
+        The returned string will have each byte separated by a space
+        Each byte must be two characters in length
+    """
+
+    # set up variables
+    HEX_DIGITS = "0123456789ABCDEF"  # hex digits table
+    NEGATIVE = False  # flag if nun is negative or not
+
+    # if num is negative --> make num a postive so we can convert it
+    # will add negative sign at end
+    if num < 0:
+        NEGATIVE = True
+        num = abs(num)
+
+    # check if endian is vaild --> if not, return None
+    if endian != "big" and endian != "little":
+        return None
+
+    hex_num = ""  # store hex number we get from num
+
+    # if num is 0, return 0
+    if num == 0:
+        hex_num = "0"
+        return hex_num
+
+    # convert num to hex and assign it to hex_num
+    while num > 0:
+        r = num % 16
+        hex_num = HEX_DIGITS[r] + hex_num
+        num //= 16
+
+    # pad hex_num with leading 0 if uneven
+    if len(hex_num) % 2 != 0:
+        hex_num = "0" + hex_num
+
+    # add spaces
+    add_spaces = ""
+    for i in range(len(hex_num)):
+        if i > 0 and i % 2 == 0:
+            add_spaces += " "
+            add_spaces += hex_num[i]
+        else:
+            add_spaces += hex_num[i]
+
+    hex_num = add_spaces
+
+    # if endian flag is little --> reverse the byte order
+    if endian == "little":
+        reverse = hex_num.split()
+        hex_num = reverse[::-1]
+        hex_num = " ".join(hex_num)
+
+    # add back negative sign if num was negative
+    if NEGATIVE:
+        hex_num = "-" + hex_num
+
+    return hex_num
